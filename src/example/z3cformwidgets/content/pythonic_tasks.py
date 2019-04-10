74262b88db1797ed018052eaae2b45a0283afc0c
MrTango <md@derico.de>
2019-04-10 19:01:45 +0300

    Add example with LinkFieldWidget

--- src/example/z3cformwidgets/content/pythonic_tasks.py
+++ src/example/z3cformwidgets/content/pythonic_tasks.py
@@ -2,10 +2,14 @@
 from plone.app.textfield import RichText
 from plone.autoform import directives
 from plone.dexterity.content import Item
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
 from plone.namedfile import field as namedfile
 from plone.supermodel import model
 from plone.supermodel.directives import fieldset
+from z3c.relationfield.schema import RelationChoice
+from z3c.relationfield.schema import RelationList
 from z3c.form.browser.radio import RadioFieldWidget
+from plone.app.vocabularies.catalog import CatalogSource
 from zope import schema
 from zope.interface import implementer
 from example.z3cformwidgets import _
@@ -14,41 +18,20 @@ from example.z3cformwidgets import _
 class IPythonicTasks(model.Schema):
     """ Marker interface and Dexterity Python Schema for PythonicTasks
     """
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    directives.widget(
+        'attachments',
+        RelatedItemsFieldWidget,
+    )
+    attachments = RelationList(
+        title=_(u'Attachments'),
+        value_type=RelationChoice(
+            title=_(u'Related'),
+            source=CatalogSource(
+                portal_type=['File'],
+            ),
+        ),
+        required=False,
+    )
 
 
 @implementer(IPythonicTasks)
